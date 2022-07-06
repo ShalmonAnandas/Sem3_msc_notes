99ac7faa35709096b0ca26fa56745209a9aab0ca
ShalmonAnandas <nashalanandas@gmail.com>
2022-07-06 12:43:00 +0530

    perl files added

---
+++ perl/raised.pl
@@ -0,0 +1,4 @@
+
+$exp = 3**5;
+
+print("3 raised to 5 is $exp\n");
---
+++ perl/viva.pl
@@ -0,0 +1,9 @@
+$a = 60;
+$b = 78;
+$c = 88;
+$d = 65;
+$e = 68;
+
+$avg = ($a+$b+$c+$d+$e) / 5;
+
+print("\nThe average is $avg\n\n");
